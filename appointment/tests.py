b86d2edad9819587a1a2f8a0d240204c95bd7f8f
Mehdi Raza <mr@berkeley-assets.com>
2023-05-27 15:26:25 +0400

    adding dummy test

--- appointment/tests.py
+++ appointment/tests.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Patient
+
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpass',
+            first_name='Test',
+            last_name='User',
+            email="test@gmail.com"
+        )
+        self.patient = Patient.objects.create(
+            user=self.user,
+            gender="male")
+
+    def test_patient_str(self):
+        self.assertEqual(str(self.patient), 'Test User')
