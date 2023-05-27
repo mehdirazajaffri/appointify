87fd44f699a186d2505ccc17844028491e66e6c8
Mehdi Raza <mr@berkeley-assets.com>
2023-05-27 14:38:30 +0400

    updating readme

--- appointment/management/commands/create_superuser.py
+++ appointment/management/commands/create_superuser.py
@@ -6,9 +6,9 @@ class Command(BaseCommand):
     help = 'Create a superuser automatically'
 
     def handle(self, *args, **options):
-        User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@example.com', 'Mehdi123')
+        get_user_model()
+        if not get_user_model().objects.filter(username='admin').exists():
+            get_user_model().objects.create_superuser('admin', 'admin@example.com', 'Mehdi123')
             self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
         else:
             self.stdout.write(self.style.WARNING('Superuser already exists'))
